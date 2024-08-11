5729a11c697e310bbfda23234ae5e473f72e49e0
Cihat <cihat_erdem@hotmail.de>
2024-08-11 18:25:06 +0200

    save2

---
+++ TradingJournal.API.dotnet/TradingJournal.API/Migrations/20240320094628_assettypemove.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TradingJournal.Migrations
+{
+    /// <inheritdoc />
+    public partial class assettypemove : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssetType",
+                table: "Transactions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AssetType",
+                table: "Positions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssetType",
+                table: "Positions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AssetType",
+                table: "Transactions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ TradingJournal.API.dotnet/TradingJournal.API/Migrations/20240530170149_updatemodels.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TradingJournal.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatemodels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransactionDate",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Total",
+                table: "Transactions",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Tax",
+                table: "Transactions",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Quantity",
+                table: "Transactions",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Transactions",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                table: "Transactions",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Commission",
+                table: "Transactions",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Transactions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Transactions",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Positions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Positions",
+                type: "boolean",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Positions");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Positions");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Total",
+                table: "Transactions",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Tax",
+                table: "Transactions",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Quantity",
+                table: "Transactions",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Transactions",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                table: "Transactions",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Commission",
+                table: "Transactions",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TransactionDate",
+                table: "Transactions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
